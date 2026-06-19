58aa0122dbff0a9dd881ae6159301182ff5ac78a
shaur <sigmashaurya2@gmail.com>
2026-06-19 22:32:36 +0530

    fix: ignore maath typescript types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useRef, useMemo } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
+// @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 import Image from "next/image";
 import Link from "next/link";
